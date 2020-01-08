a4dcd56f3eccfbbca6218350754d702a1b0b228a
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-08 14:19:01 -0400

    Estrutura base do projeto Task List adicionada.

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: ['@babel/polyfill', 'whatwg-fetch', './js/script.js'],
+    output: {
+        path: path.resolve(__dirname, './js/'),
+        filename: 'main.js'
+    }
+};
